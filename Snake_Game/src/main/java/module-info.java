25782598a2e406c802f6026212aad0140c59de5f
Nguyenvantruc <truc.nv.2296@aptechlearning.edu.vn>
2024-01-30 00:41:27 +0700

    loginDisplay

--- Snake_Game/src/main/java/module-info.java
+++ Snake_Game/src/main/java/module-info.java
@@ -4,7 +4,8 @@ module com.example.snake_game {
     requires javafx.fxml;
     requires mongo.java.driver;
 
-    opens com.example.snake_game.views;
+    opens com.example.snake_game.controllers;
     opens com.example.snake_game to javafx.fxml;
+    exports com.example.snake_game.controllers;
     exports com.example.snake_game.views;
 }
