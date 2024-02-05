efb1e28785733b13cf52c39da0a5a29c1d1db91b
trinhvanvy <trinhvanvy2000@gmail.com>
2024-02-06 00:08:56 +0700

    update display

--- Snake_Game/src/main/java/com/example/snake_game/controllers/DisplayController.java
+++ Snake_Game/src/main/java/com/example/snake_game/controllers/DisplayController.java
@@ -3,37 +3,70 @@ package com.example.snake_game.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class DisplayController {
-    @FXML
-    private Button playButton;
 
     @FXML
-    private Button skinsButton;
+    private BorderPane mainPane;
 
     @FXML
-    private Button rankingButton;
+    private HBox topHBox;
 
     @FXML
-    private Button settingsButton;
+    private HBox leftVBox;
 
     @FXML
-    private void handlePlayButtonAction(ActionEvent event) {
-        // Xử lý sự kiện khi nhấn nút "Chơi"
+    private VBox rightVBox;
+
+    @FXML
+    private ImageView UserImage;
+
+    @FXML
+    private ImageView SettingImage;
+
+    @FXML
+    private ImageView RankingImage;
+
+    @FXML
+    private ImageView EventImage;
+
+    @FXML
+    private Button StartButton;
+
+    @FXML
+    private Button SkinsButton;
+
+    @FXML
+    private void handleUserImageClick(ActionEvent event) {
+        // Xử lý sự kiện khi hình ảnh User được nhấp vào
     }
 
     @FXML
-    private void handleSkinsButtonAction(ActionEvent event) {
-        // Xử lý sự kiện khi nhấn nút "Skins"
+    private void handleSettingImageClick(ActionEvent event) {
+        // Xử lý sự kiện khi hình ảnh Setting được nhấp vào
     }
 
     @FXML
-    private void handleRankingButtonAction(ActionEvent event) {
-        // Xử lý sự kiện khi nhấn nút "Xếp Hạng"
+    private void handleRankingImageClick(ActionEvent event) {
+        // Xử lý sự kiện khi hình ảnh Ranking được nhấp vào
     }
 
     @FXML
-    private void handleSettingsButtonAction(ActionEvent event) {
-        // Xử lý sự kiện khi nhấn nút "Cài Đặt"
+    private void handleEventImageClick(ActionEvent event) {
+        // Xử lý sự kiện khi hình ảnh Event được nhấp vào
+    }
+
+    @FXML
+    private void handleStartButtonClick(ActionEvent event) {
+        // Xử lý sự kiện khi nút Start được nhấp vào
+    }
+
+    @FXML
+    private void handleSkinsButtonClick(ActionEvent event) {
+        // Xử lý sự kiện khi nút Skins được nhấp vào
     }
 }
