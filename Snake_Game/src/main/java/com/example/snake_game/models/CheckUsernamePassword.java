ed400c07ff0532d252bf4067a7b30146ba267e87
huongcaoha <huongcaoha1994@gmail.com>
2024-02-09 21:33:05 +0700

    update code BE login & register

---
+++ Snake_Game/src/main/java/com/example/snake_game/models/CheckUsernamePassword.java
@@ -0,0 +1,33 @@
+package com.example.snake_game.models;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class CheckUsernamePassword {
+    public static boolean checkUsernamePassword(String username,String password){
+        MongoDatabase database = null;
+        // Thay đổi URI kết nối theo cấu hình MongoDB của bạn
+        String connectionString = "mongodb://localhost:27017/?retryWrites=true&w=majority";
+        boolean checkUsername = false ;
+        try (MongoClient mongoClient = new MongoClient(new MongoClientURI(connectionString))) {
+            // Kết nối thành công
+
+            // Chọn cơ sở dữ liệu
+            database = mongoClient.getDatabase("kingsnake");
+            MongoCollection<Document> collection = database.getCollection("user");
+            Document query = new Document("username", username).append("password", password);
+            Document rs = collection.find(query).first();
+            if(rs != null){
+                checkUsername = true;
+            }
+
+        } catch (Exception e) {
+            // Xử lý lỗi kết nối
+            e.printStackTrace();
+        }
+        return checkUsername;
+    }
+}
