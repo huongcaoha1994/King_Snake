6454da3f459a1bc51e512e17f6b24226f1434b0c
huongcaoha <huongcaoha1994@gmail.com>
2024-03-07 09:55:43 +0700

    fix bug

--- Snake_Game/src/main/java/com/example/snake_game/views/Main.java
+++ Snake_Game/src/main/java/com/example/snake_game/views/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         primaryStage.setScene(SceneLogin.login());
+        primaryStage.setResizable(false);
         primaryStage.show();
 
 
